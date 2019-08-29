9a2affe40802a84d16c6779d57e426c53a733524
Lamees AlFallaj <lafallaj2@gmail.com>
2019-08-29 18:56:41 +0300

    function on site

--- js/main.js
+++ js/main.js
@@ -3,21 +3,24 @@ console.log('up and running');
 var cards = ['queen', 'queen', 'king', 'king'];
 var cardInPlay = [];
 
-var cardOne = cards[0];
+function checkForMatch() {
+    if (cardInPlay.length === 2) {
+        if (cardInPlay[0] === cardInPlay[1]) {
+            // alert('we have a wiiiinnner')
+            console.log('we have a wiiiinnner')
 
-cardInPlay.push(cardOne);
-
-var cardTwo = cards[2];
-
-cardInPlay.push(cardTwo);
-
-if (cardInPlay.length === 2) {
-    if (cardInPlay[0] === cardInPlay[1]) {
-        alert('we have a wiiiinnner')
-        console.log('we have a wiiiinnner')
-
-    } else {
-        console.log('at least you\'ve tried')
-        alert('at least you\'ve tried')
+        } else {
+            console.log('at least you\'ve tried')
+                // alert('at least you\'ve tried')
+        }
     }
 }
+
+function flipCard(cardID) {
+    console.log(cards[cardID] + ' is fipped')
+    cardInPlay.push(cardID)
+    checkForMatch();
+}
+
+flipCard(0);
+flipCard(2);
