822b1cc6f66a7560e62eab3387e2b868775150f0
Lamees AlFallaj <lafallaj2@gmail.com>
2019-08-29 18:41:25 +0300

    added the array and if statment

--- js/main.js
+++ js/main.js
@@ -1,8 +1,23 @@
 console.log('up and running');
 
-var cardOne = 'Queen';
-var cardTwo = 'King';
-var cardThree = 'Queen';
-var cardFour = 'King';
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardInPlay = [];
 
-console.log(cardFour + ' flipped');
+var cardOne = cards[0];
+
+cardInPlay.push(cardOne);
+
+var cardTwo = cards[2];
+
+cardInPlay.push(cardTwo);
+
+if (cardInPlay.length === 2) {
+    if (cardInPlay[0] === cardInPlay[1]) {
+        alert('we have a wiiiinnner')
+        console.log('we have a wiiiinnner')
+
+    } else {
+        console.log('at least you\'ve tried')
+        alert('at least you\'ve tried')
+    }
+}
